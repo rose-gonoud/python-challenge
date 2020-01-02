799bd3339d206a998bb0e8c92d771783fd14adb4
Rose <rose.gonoud@gmail.com>
2020-01-01 20:58:33 -0800

    first challenge complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,22 +9,52 @@ with open(csvPath, newline='') as csvfile:
     csv_header = next(csvreader)
     monthsTotal = 0
     netProfitLosses = 0
+    lastMonthPL = 0
     avgChngProfitLoss = 0
+    profitLossDiffs = 0
     greatestIncProfit = 0
+    currentHighestProfit = 0
     greatestDecLoss = 0
+    currentLoss = 0
 
     for row in csvreader:
         monthsTotal = monthsTotal + 1
-
         netProfitLosses = netProfitLosses + int(row[1])
+      
+        # Once there's been more than one month, we can take this diff,
+        #   otherwise one counts the jump from 0 to the first month's profit.
+        if monthsTotal > 1:
+            profitLossDiffs = profitLossDiffs + (lastMonthPL - int(row[1]))
 
+        # This makes last month's profits, defined at first as 0, reflect the row that
+        #   just passed thru the profitLossDiffs eq. Allows one to access previous row easily.
+        lastMonthPL = int(row[1])
 
+        if int(row[1]) > greatestIncProfit:
+            greatestIncProfit = int(row[1])
+            date1 = row[0]
 
-    #output_file = os.path.join("PyBank.txt")
-    #with open(output_file, "w", newline="") as datafile:
-     #   writer = text.writer(datafile)
-      #  writer.writerow(["Total Months", "Total", "Average Change", "Greatest Increase in Profits", "Greatest Decrease in Losses"])
-       # writer.writerows(roster)
+        if int(row[1]) < greatestDecLoss:
+            greatestDecLoss = int(row[1])
+            date2 = row[0]
 
-print(monthsTotal)
-print(netProfitLosses)
+    # Writing this outside of the loop to not waste time computing avgs for each line.
+    avgChngProfitLoss = round(profitLossDiffs/monthsTotal, 2)
+    
+
+    output = "Financial Analysis \n\n"
+    output += "---------------------------------- \n"
+
+    output += f"Total Months: {monthsTotal} \n"
+    output += f"Total: ${netProfitLosses} \n"
+    output += f"Average Change: ${avgChngProfitLoss} \n"
+
+    output += f"Greatest Increase in Profits: {date1} (${greatestIncProfit}) \n"
+    output += f"Greatest Decrease in Losses: {date2} (${greatestDecLoss}) \n"
+    output += "----------------------------------"
+
+    print(output)
+
+    f = open("PyBank.txt", "w")
+    f.write(output)
+    f.close()
